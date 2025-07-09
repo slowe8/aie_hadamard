87cdcb213a2f2369f885ea2c308fd6af36761f75
Sean Lowe <slowe8@asu.edu>
2025-07-09 04:18:11 +0000

    Added gate enum definitions

---
+++ aie/src/gate1q.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+enum struct Gate1Q {
+	I,		// Identity
+	X,		// PauliX
+	Y,		// PauliY
+	Z,		// PauliZ
+	H,		// Hadamard
+	S,		// Phase
+	SDG,	// S inverse
+	T,		// pi/8
+	TDG,	// T inverse
+};
+
---
+++ aie/src/gate2q.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+enum struct Gate2Q {
+	CX,		// Controlled X (CNOT)
+	CY,		// Controlled Y
+	CZ,		// Controlled Z
+	CH,		// Controlled Hadamard
+	SWAP,	// SWAP gate
+	ZZ,		
+	XX,
+	YY
+};
+
